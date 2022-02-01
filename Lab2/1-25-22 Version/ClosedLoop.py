d877f83b9a552d4f774ff2ff5230f9d127f27266
dcejagon <dcejagon@calpoly.edu>
2022-01-31 17:55:21 -0800

    Update ClosedLoop.py

--- Lab2/1-25-22 Version/ClosedLoop.py
+++ Lab2/1-25-22 Version/ClosedLoop.py
@@ -1,5 +1,17 @@
-
+"""
+    @file                    ClosedLoop.py
+    @brief                   This file controls the output speed of the motor
+    
+    @author                  Daniel Gonzalez
+    @author                  Nolan Clapp
+    @author                  Caleb Kephart
+    @date                    January 31, 2022
+"""
 class ClosedLoop:
+    
+    '''@brief       Creates a class for closed loop control on the motor speed
+       @details     P-controller drives the measured value to the Setpoint  
+    '''
     
     def __init__(self,Kp,setpoint,EncPosition,duty):
         self.Kp=Kp
@@ -11,8 +23,11 @@ class ClosedLoop:
         self.setpoint=setpoint.read()
         
     def SetKp(self,Kp):
-        self.Kp=Kp.read()
         
+        '''@brief   sets the Kp value
+        '''
+        
+        self.Kp=Kp.read()
             
     def control_loop(self):
         self.error=self.EncPosition.read()-self.setpoint.read()

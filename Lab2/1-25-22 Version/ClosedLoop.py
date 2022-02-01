23ab563e908d48b490b4b51cce62cf5e8f7b6a24
cmkep <97115665+cmkep@users.noreply.github.com>
2022-01-31 22:05:04 -0800

    Create ClosedLoop.py

---
+++ Lab2/1-25-22 Version/ClosedLoop.py
@@ -0,0 +1,59 @@
+"""
+    @file                    ClosedLoop.py
+    @brief                   This file provides the code to interface directly with the encoder hardware
+    @author                  Daniel Gonzalez
+    @author                  Nolan Clapp
+    @author                  Caleb Kephart
+    @date                    January 31, 2022
+"""
+import utime
+import time
+#from matplotlib import pyplot as plt
+
+class ClosedLoop:
+    
+    def __init__(self,Kp,setpoint,EncPosition,duty,time):
+        self.Kp=Kp
+        self.setpoint=setpoint
+        self.EncPosition=EncPosition
+        self.duty=duty
+        self.time=time
+        self.Time=[]
+        self.Pos=[]
+        self.starttime=time.ticks_ms()
+    def Setpoint(self,setpoint):
+        self.setpoint=setpoint.read()
+        
+    def SetKp(self,Kp):
+        self.Kp=Kp.read()
+        
+            
+    def control_loop(self):
+        self.error=self.EncPosition.read()-self.setpoint.read()
+        self.actuation=self.Kp.read()*self.error
+        
+        self.duty.write(self.actuation)
+        
+        
+        utime.sleep_ms(10)
+        #print(time.ticks_ms(),self.EncPosition.read())
+        
+        self.Time.append(time.ticks_diff(time.ticks_ms(),self.starttime))
+        self.Pos.append(self.EncPosition.read())
+    # def printdata(self):
+        
+    #     print(self.Time,self.Pos)
+        
+    def printdata(self):
+        n=0
+        while n< len(self.Time):
+            print(self.Time[n],self.Pos[n])
+            n=n+1
+     
+        
+        
+    
+    
+    
+        
+

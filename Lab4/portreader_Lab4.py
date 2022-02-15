80d2814deef1556c02960a6392752658caf24506
nzclapp <nzclapp@calpoly.edu>
2022-02-14 16:47:44 -0800

    readme and fix vertical axis lable

--- Lab4/portreader_Lab4.py
+++ Lab4/portreader_Lab4.py
@@ -28,16 +28,8 @@ with serial.Serial ('COM26', 115200) as ser_port:
         except KeyboardInterrupt:
             break
         
-
 data_results = ','.join(data_results).split(',')
 print(data_results)   
-
-
-
-
-
-# listOdd = data_results[1::2] # Elements from list1 starting from 1 iterating by 2
-# listEven = data_results[::2] # Elements from list1 starting from 0 iterating by 2
 
 data_results.pop(len(data_results)-1)
 data_results.pop(0)
@@ -46,23 +38,14 @@ for i in range(0, len(data_results)):
    data_results[i] = int(data_results[i])
 
 Time = range(0,len(data_results))
-#print (listOdd)
-#print (listEven)
-   
-# item_x = [i.split(',', 1)[0] for i in data_results]
-# print(item_x)        
-   
-# item_y = [i.split(',', 1)[0] for i in data_results]
-# print(item_y)  
-# 
+
 
 plt.scatter(Time,data_results)
 plt.plot(Time,data_results)
 
-
 # Axis Labeling
 plt.xlabel('Time (ms)') 
-plt.ylabel('DAC Step Response (ticks)') 
+plt.ylabel('ADC Step Response (ticks)') 
     
 # Graph Title
 plt.title('Lab 4 Plots') 
